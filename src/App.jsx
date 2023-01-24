92aceea034e2ca2c17441be244d00728b4642bea
Zien Alhaoushe <zienalhaoushe@hotmail.com>
2023-01-24 15:10:49 +0200

    Trivia app initial commit

---
+++ src/App.jsx
@@ -0,0 +1,157 @@
+
+import React from "react"
+import Question from "./Question"
+import { nanoid } from 'nanoid'
+import { DOMParser } from 'xmldom'
+
+function App() {
+  const [checked, setCheck] = React.useState(false)
+  const [result, increaseRes] = React.useState(0)
+  const [questions, setQuestions] = React.useState([])
+  const [answers, setAnswerID]=React.useState("")
+  console.log(answers)
+
+  function startQuiz(){
+    let quiz = []   
+    
+    fetch("https://opentdb.com/api.php?amount=5")
+    .then(res => res.json())
+    .then(data =>{
+      data.results
+          .map(ele=>{
+            const allAnswers = [...ele.incorrect_answers, ele.correct_answer]
+            const shuffledAnswers = shuffleArray(allAnswers)
+            quiz.push({
+              id : nanoid(),
+              category: ele.category,
+              incorrect_answers: ele.incorrect_answers,
+              correct_answer:ele.correct_answer,
+              all_answers: shuffledAnswers,
+              type:ele.type,
+              question: ele.question,
+              btnColor: {backgroundColor: "transparent"},
+              selected:""
+              
+      
+            })
+          }
+            
+     )
+      
+      setQuestions(quiz)
+
+    })
+
+  }
+
+  function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array // be sure to return the array!
+  }
+
+  console.log(questions)
+
+  function setAnswer(id,val){
+    // questions.map(ele=>{
+    
+    // })
+    setAnswerID(id)
+
+    console.log("clicked!")
+    setQuestions(prevQuestions => {
+      const updatedQuestions = prevQuestions.map(question => {
+        if(question.id === id) {
+          return {
+            ...question,
+            selected: val
+          }
+        } else return question
+      })
+      
+      return updatedQuestions
+    })
+
+  }
+  function checkAnswer(){
+    console.log("Checked!")
+    questions.map(ele=>{
+      console.log(ele)
+      if(ele.correct_answer == ele.selected){
+        increaseRes(prev=> prev+1)
+
+      }
+      
+      
+      console.log(questions)
+
+      setCheck(true)
+   
+
+    })
+  }
+  function tryAgain(){
+
+    startQuiz()
+    increaseRes(0)
+    setCheck(false)
+
+
+  }
+  
+  return (
+    <main>
+      {questions.length>0?
+      <div className="btn-parent">
+        {questions && questions.length && questions.map((question,id)=>{
+      return <div className="questions_page" key={id}>
+              <Question 
+              key = {question.id}
+              category={question.category} 
+              question ={question.question}
+                correct_answer ={question.correct_answer}
+                incorrect_answers = {question.incorrect_answers}
+                all_answers = {question.all_answers}
+                id={question.id}
+                toggle = {question.toggle}
+                setAnswer = {setAnswer}
+                color = {question.btnColor}
+                selected= {question.selected}
+                checked= {checked}
+                setQuestions = {setQuestions}
+                />
+      </div>
+    })}
+            {checked?
+              <div className="quest_query checking">
+                <h3 >You scored {result}/5 correct answers </h3>
+               <button className="first-btn btn try" onClick={tryAgain}>Try again</button>
+              </div>:
+            <button className="first-btn btn" onClick={checkAnswer}>Check answers</button>
+
+
+             
+
+               }
+            
+
+      </div>:
+     
+        <div className="no-quiz">
+          <h1>Quizzical</h1>
+          <p>Some description if needed</p>
+          <button className="first-btn " onClick={startQuiz}>Start quiz</button>
+      </div>
+    }
+
+    </main>
+
+  )
+}
+// onClick={startQuiz}
+export default App
+//https://opentdb.com/api.php?amount=5 API link
---
+++ src/Question.jsx
@@ -0,0 +1,99 @@
+import React from "react"
+import { DOMParser } from 'xmldom'
+import { nanoid } from 'nanoid'
+let userAnswers = []
+
+export default function Question(props){
+    let answer = props.all_answers
+      function decodeHtml(html) {
+        var txt = document.createElement('textarea');
+        txt.innerHTML = html;
+        return txt.value;
+      }
+
+    function setSelected(val, ele){
+        console.log(val)
+        val = ele
+        console.log(val)
+        return val
+
+    }
+      
+    
+    return (
+        <div className="quest_query">
+           { props.checked?
+           
+           
+           <div>
+  <h3>{
+               decodeHtml( props.question)
+            }</h3>
+            <div className="answers"> 
+              
+              {answer.map((ele,index)=> {
+                let color= {backgroundColor:"transparent"}
+                if(props.selected == ele){
+                    if(props.correct_answer == ele){
+                        color ={backgroundColor:"#94D7A2"}
+                    }
+                    else{
+                        color ={backgroundColor:"#F8BCBC"}
+                    }
+                }
+                else {
+                    if (props.correct_answer === ele) {
+                        color = { backgroundColor: "#94D7A2" }
+                    }
+                }
+                
+
+        
+          
+                return   (
+                    <p className="answer" style={color} key={index}  >
+                    {ele}</p>
+                )
+            }
+          
+           )}
+
+              
+            
+            </div>
+
+
+           </div>
+           
+           
+           :<div>
+
+           <h3>{
+               decodeHtml( props.question)
+            }</h3>
+            <div className="answers"> 
+              
+              {answer.map((ele,index)=> {
+                let color= {backgroundColor:"transparent"}
+                if(props.selected == ele){
+                    color ={backgroundColor:"#D6DBF5"}
+                }
+                return   <p className={`answer`} style={color} key={index} onClick={()=> props.setAnswer(props.id, ele)} >{ele}</p>
+            }
+          
+           )}
+
+              
+            
+            </div>
+        
+
+
+           </div> }
+
+          
+        
+        </div>
+        
+    )
+}
